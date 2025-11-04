36f8709255e87d133662ff05f78c426a6f9c99e9
AlejoVLenis <alejandro.v_lenis@uao.edu.co>
2025-11-04 11:59:20 +0000

    Nueva pagina formulario

---
+++ Pagina Proyecto Final Introducción/js/main.js
@@ -0,0 +1,7 @@
+let ofertas = document.querySelectorAll(".btn-empleo");
+ofertas.forEach(element => {
+    addEventListener('click', function(){
+        window.location.href = "formulario-ofertas.html"
+    })
+});
+
